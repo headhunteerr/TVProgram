9a946e214ed72ce3f3953fad0215cdf8a42a1783
headhunteerr <nicolas.mandadjiev@gmail.com>
2018-06-26 17:27:12 +0200

    Application

--- src/main/java/com/tv/program/Application.java
+++ src/main/java/com/tv/program/Application.java
@@ -1,5 +1,6 @@
 package com.tv.program;
 
+import com.tv.program.Filtres.ProgrammeFiltre;
 import com.tv.program.model.Chaine;
 import com.tv.program.model.programmes.Programme;
 import com.tv.program.parser.ProgrammeLoader;
@@ -7,172 +8,145 @@ import com.tv.program.parser.ProgrammeLoader;
 import com.tv.program.windows.FenetreChaines;
 import com.tv.program.windows.FenetreListeJours;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.util.*;
 
-public class Application extends Frame{
+public class Application{
+
+    private boolean running = true;
 
     private List<Programme> programmes = new ArrayList<>();
     private List<Chaine> chaines = new ArrayList<>();
+    private List<String> commands = new ArrayList<>();
 
-    Application(){
-        try (InputStream inputStream = ProgrammesCounter.class.getResourceAsStream("/tvguide.xml")){
+    Application() {
+        try (InputStream inputStream = ProgrammesCounter.class.getResourceAsStream("/tvguide.xml")) {
             ProgrammeLoader.load(inputStream, chaines, programmes);
         } catch (IOException e) {
             throw new RuntimeException("Erreur", e);
         }
+        commands.add("help");
+        commands.add("chaines");
+        commands.add("joursProgrammes");
+        commands.add("listeProgrammes");
+        commands.add("infosEmission");
+        commands.add("quit");
 
-        setSize(400,200);
-        this.setTitle("Consultation des programmes");
-        this.setLocation((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2-this.getWidth()/2,(int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2-this.getHeight()/2);
-        this.setBackground(Color.gray);
-        setLayout(null);
-        setVisible(true);
-
-        Button listeChaines = new Button("Liste des chaines");
-        listeChaines.setSize(120,30);
-        listeChaines.setLocation(this.getWidth()/2-listeChaines.getWidth()/2,listeChaines.getHeight());
-        listeChaines.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                FenetreChaines fenetreChaines = new FenetreChaines(chaines);
-                fenetreChaines.addWindowListener(new WindowListener() {
-                    @Override
-                    public void windowOpened(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowClosing(WindowEvent e) {
-                        fenetreChaines.dispose();
-                    }
-
-                    @Override
-                    public void windowClosed(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowIconified(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowDeiconified(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowActivated(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowDeactivated(WindowEvent e) {
-
-                    }
-                });
-            }
-        });
-        add(listeChaines);
-
-        Button listeJours = new Button("Liste des jours disposant de programmes");
-        listeJours.setSize(250,30);
-        listeJours.setLocation(this.getWidth()/2-listeJours.getWidth()/2,listeChaines.getHeight()+listeJours.getHeight());
-        listeJours.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                FenetreListeJours fenetreListeJours = new FenetreListeJours(programmes);
-                fenetreListeJours.addWindowListener(new WindowListener() {
-                    @Override
-                    public void windowOpened(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowClosing(WindowEvent e) {
-                        fenetreListeJours.dispose();
-                    }
-
-                    @Override
-                    public void windowClosed(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowIconified(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowDeiconified(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowActivated(WindowEvent e) {
-
-                    }
-
-                    @Override
-                    public void windowDeactivated(WindowEvent e) {
-
-                    }
-                });
-            }
-        });
-        add(listeJours);
-
-        this.addWindowListener(new WindowListener() {
-            @Override
-            public void windowOpened(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowClosing(WindowEvent e) {
-                dispose();
-            }
-
-            @Override
-            public void windowClosed(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowIconified(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowDeiconified(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowActivated(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowDeactivated(WindowEvent e) {
-
-            }
-        });
+        commands = Collections.unmodifiableList(commands);
     }
 
     public List<Programme> getProgrammes(){return this.programmes;}
 
     public List<Chaine> getChaines(){return this.chaines;}
 
+    public List<String> getCommands(){return this.commands;}
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void start(){
+        System.out.println("** Bienvenue dans l'application de programme TV **");
+        System.out.println("** Tapez \'help\' pour avoir la liste des commandes **");
+        this.running = true;
+    }
+
+    public void help(){
+        System.out.println("// Liste des commandes:\n" +
+                "// chaines : liste toutes les chaines contenues dans le programme TV \n" +
+                "// joursProgrammes : liste de tous les jours disposant d'un programme TV \n" +
+                "// quit : quitter l'application \n" +
+                "// listeProgrammes -'chaine' -jj/mm/aaaa : liste des programmes sur la chaine 'chaine' a la date choisie \n"
+        );
+
+    }
+
+    public void printChannels(){
+        for (Chaine chaine:chaines){
+            System.out.println(chaine.toString());
+        }
+    }
+
+    public void printDaysWithPrograms(){
+        List<Date> jours = ProgrammeFiltre.listeJour(programmes);
+        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.FRANCE);
+        for (Date jour:jours){
+            System.out.println(df.format(jour));
+        }
+    }
+
+    public void printChannelProgram(String command){
+        String[] commandTab = command.split("-");
+        for(int i = 0; i <commandTab.length; i++){
+            commandTab[i] = commandTab[i].trim();
+        }
+        String chaine = commandTab[1];
+        String[] dateString = commandTab[2].split("/");
+        for(String string:dateString){
+            System.out.println(string);
+        }
+        int[] dateInts = new int[dateString.length];
+        for(int i = 0; i < dateString.length; i++){
+            dateInts[i] = Integer.parseInt(dateString[i]);
+        }
+        if (dateInts[2]>0 && (dateInts[1]>0 && dateInts[2]<=12) && (dateInts[0]>0 && dateInts[0]<=31)) {
+            Date date = new Date(dateInts[2], dateInts[1], dateInts[0], 0, 0);
+            List<Programme> programmes = ProgrammeFiltre.listeProgrammeChaine(this.getProgrammes(), date, chaine);
+            for (Programme p : programmes) {
+                p.toString();
+            }
+        }
+    }
+
+    public void printBrodcastInfo(String command){
+        String[] commandTab = command.split("-");
+        for(int i = 0; i <commandTab.length; i++){
+            commandTab[i] = commandTab[i].trim();
+        }
+
+    }
+
+    public void stop(){
+        this.running = false;
+    }
+
+
     public static void main(String[] args) {
         Application a = new Application();
+        a.start();
+
+        while(a.isRunning()){
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String commandString ="itialization";
+            try{
+                commandString = br.readLine();
+            }
+            catch (Exception e){
+                System.err.println("Not a string");
+            }
+            int command = a.getCommands().indexOf(commandString.split(" ")[0]);
+            if (command!=-1){
+                switch (command){
+                    case 0: a.help();break;
+
+                    case 1: a.printChannels();break;
+
+                    case 2: a.printDaysWithPrograms();break;
+
+                    case 3: a.printChannelProgram(commandString);break;
+
+                    case 4: a.printBrodcastInfo(commandString);break;
+
+                    case 5: a.stop();break;
+                }
+            }
+            else{
+                System.out.println("** Commande invalide! Tapez \'help\' pour obtenir la liste des commandes **");
+            }
+        }
     }
 }
